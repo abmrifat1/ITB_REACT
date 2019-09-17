273fa16df49eb4d6e846783a8a952b65268ca3df
abmrifat1 <rifatabm1@gmail.com>
2019-09-17 21:00:47 +0600

    day3last

--- day3.js
+++ day3.js
@@ -68,3 +68,92 @@ function getMonth(month){
 }
 console.log(getMonth(3))
 
+function getMonth(month){
+    switch(month){
+        case 1:return "january";
+        break;
+        case 2:return "february";
+        break;
+        case 3:return "martch";
+        break;
+        case 4:return "april";
+        break;
+        case 5:return "may";
+        break;
+        default:return "not match";
+    }
+
+}
+console.log(getMonth(5));
+
+function getTeam(name){
+    switch (name){
+        case 'abm':
+        case 'rifat':
+        case 'alam':return "team A member";
+        break;
+        case 'ars':
+        case 'rouf':
+        case 'heron':return "team B member";
+        break;
+        default:return("not match");
+        break;
+    }
+}
+
+console.log(getTeam('abm'))
+
+function findMan(prop){
+    var man={
+        name:"abm",
+        id:5998,
+        cgp:3.77
+    }
+    return man[prop];
+}
+console.log(findMan("name"))
+
+
+function login(name,password,type){
+    var credential={
+        name:"",
+        password:"",
+        type:""
+    }
+   credential["name"]=name;
+   credential["password"]=password;
+   credential["type"]=type;
+
+   return credential;
+}
+console.log(login("abm","123","basic"))
+
+var profile={
+    name:"Khorshed Alam",
+    education:[{
+        instuteType:"ssc",
+        instuteName:"bhs",
+        instuteAddress:"chadpur",
+        passyear:"2011"
+    },{
+        instuteType:"hsc",
+        instuteName:"sc",
+        instuteAddress:"dhaka",
+        passyear:"2013"
+    },{
+
+        instuteType:"bsc",
+        instuteName:"DIU",
+        instuteAddress:"dhaka",
+        passyear:"2019"
+    }],
+    phone:['01772515721','01738366238'],
+    address:{
+        city:"baridara",
+        lane:"13",
+        hpuse:"540/6"
+
+    }
+}
+
+console.log(profile.educationInstute.ssc);
