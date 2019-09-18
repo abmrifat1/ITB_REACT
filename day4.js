6e159b4cfb8b4ab15bae74501aadc7cb6ef086b1
abmrifat1 <rifatabm1@gmail.com>
2019-09-18 12:57:25 +0600

    day-4

---
+++ day4.js
@@ -0,0 +1,12 @@
+function leafYear(year){
+    if(year%4==0){
+        console.log(year+" Year is a leafyear")
+    }
+    else{
+        console.log("not a leafyear");
+
+    }
+}
+
+leafYear(2000);
+leafYear(2019);
