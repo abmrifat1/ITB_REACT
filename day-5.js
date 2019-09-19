72e8a62f3e99fa77f26b68bf0a870c9a81b40e74
abmrifat1 <rifatabm1@gmail.com>
2019-09-19 19:52:36 +0600

    4th day

---
+++ day-5.js
@@ -0,0 +1,16 @@
+function name(m,n){
+    let array=[];
+
+    for(var i=0;i<m;i++){
+        var row=[];
+        for(var j=0;j<n;j++){
+            row.push(2);
+        }
+        array.push(row)
+
+    }
+
+    return array;
+}
+
+console.log(name(3,2))
