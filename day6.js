478ae0aff256328c22fbe6cb57c0ed077cc4221e
abmrifat1 <rifatabm1@gmail.com>
2019-09-21 16:02:56 +0600

    6th day

--- day6.js
+++ day6.js
@@ -21,3 +21,30 @@
     }while(i<=groceries.length-1)
 
     console.log("today shopping :"+ string)
+
+
+    var string="3646352565"
+    var number=parseInt(string, 10) 
+    console.log(number)
+
+    var string="";
+ 
+    var groceries=[
+        {item:"veg",price:"20"},{item:"banana",price:"30"}
+    ]
+    for(var i=groceries.length-1;i>=0;i--){
+        var string= string+( groceries[i].item +":"+ groceries[i].price)+", ";
+    }
+
+    console.log("today shopping :"+ string)
+    
+
+    //////////////////////////////////////////////
+
+    function profile(){
+        const name="Khorshed alam";
+        let email="rifatabm1@gmailcom";
+        let phoneNo="01772515721";
+        let address="kolabagan";
+        const birthday="1/1/1996";
+    }
