c41633e69bcf7857eb84039d7f4aefedde47b1fc
abmrifat1 <rifatabm1@gmail.com>
2019-09-22 16:19:26 +0600

    7th Day

--- day7.js
+++ day7.js
@@ -20,3 +20,38 @@ const updateEducationInfo=(currentInfo,newInfo)=>{
 }
 
 console.log(updateEducationInfo(educationInfo,newInfo));
+
+const user=[
+    {
+        name:"abm",
+        password:"abm"
+    },{
+        name:"ars",
+        password:"ars"
+    }
+]
+login=(userName,usePassword)=>{
+    for(var i=0;i<=(user.length)-1;i++ ){
+        
+        if(userName===user[i].name && usePassword===user[i].password){
+            return "you are successfully login"
+        }
+        else{
+            return "try again with valid information"
+        }
+    }
+   
+}
+console.log(login('abm','abm'))
+
+singUp=(userName,userPassword,person)=>{
+     
+  const newUser= {
+            name:userName,
+            password:userPassword
+        }
+        person.push(newUser)
+        return person;
+   
+}
+console.log(singUp('rifat','123456',user))
