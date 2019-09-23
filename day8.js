c857a990b5dc10e2d6b06a0a46b46347b31ac857
abmrifat1 <rifatabm1@gmail.com>
2019-09-23 19:36:10 +0600

    8th day final

--- day8.js
+++ day8.js
@@ -2,7 +2,7 @@
 
 
   var obj = {
-    id: 48,
+    id: "end day",
     counter: function counter() {
       setTimeout(() => {
         console.log(this.id);
@@ -33,5 +33,51 @@ function findLargestLength(str){
 
 console.log(findLargestLength(sentence))
 
+//filter a array using filter() methood
+ 
+var list=['abm','rifat','tipu','shamim','sifat'];
+const listFilter=list.filter(i=>i.length>=5);
+console.log(listFilter) 
+
+//map() methood for calcution an array
+
+var gpa=[3.4,3.77,5.00]
+
+const calcutionGpa=gpa.map(x=>x-.07)  //subtraction -.07 from all elements of array
+console.log(calcutionGpa)
+
+const bolleanGpa=gpa.map(x=>x>4)  //filter array all elements that is greater than 4
+console.log(bolleanGpa)
+
+const powerGpa=Math.pow(gpa[2],2)  // square a specific elements of array
+console.log(powerGpa)
+
+const powerFullArray=gpa.map(i=>Math.pow(i,2))   //quare all elements of array
+console.log(powerFullArray)
+
+const mark=[1,2,3,4,5]
+const mechanism=(a,b)=>a+b;
+
+console.log(mark.reduce(mechanism,20))
 
 
+
+//show the largest number of array.
+
+var number=[[0,1],[9,4,3],[33,9999,4,4,45],[455,444,454,33,44455]];
+
+function findLargestNumber(number){
+    var largestNumberArray=[];
+    for(var i=0;i<=(number.length)-1;i++){
+      var largestNumber=number[i][0];
+       for(var j=0;j<number[i].length;j++){
+         if(number[i][j]>=largestNumber){
+           largestNumber=number[i][j];
+         }
+       }
+        largestNumberArray[i]=largestNumber;
+}
+return largestNumberArray;
+}
+
+console.log(findLargestNumber(number))
