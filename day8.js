8d450fb94059c2482fec0df30cf296a311f90eb5
abmrifat1 <rifatabm1@gmail.com>
2019-09-23 17:42:06 +0600

    8th Day

--- day8.js
+++ day8.js
@@ -2,7 +2,7 @@
 
 
   var obj = {
-    id: 42,
+    id: 48,
     counter: function counter() {
       setTimeout(() => {
         console.log(this.id);
@@ -13,4 +13,25 @@
   console.log(obj.counter())
 
 
-//console.log (obj.address); 
+//console.log (obj.address);
+
+
+
+//find lenght of the largest word in  string;
+
+var sentence="my name is khorshed alam mazumder";
+function findLargestLength(str){
+  var str=sentence.split(' ');
+  var maxlenght=0;
+  for(var i=0;i<=(str.length)-1;i++){
+    if(str[i].length>=maxlenght){
+      maxlenght=str[i].length;
+    }
+  }
+  return maxlenght;
+}
+
+console.log(findLargestLength(sentence))
+
+
+
