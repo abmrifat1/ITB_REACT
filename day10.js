2b2fe0013974517dfdc0581408780c3be210856a
abmrifat1 <rifatabm1@gmail.com>
2019-09-25 16:37:33 +0600

    constractor function

--- day10.js
+++ day10.js
@@ -13,4 +13,22 @@ const LOCAL_FORECAST = {
     return maxOfTomorrow;
   }
   
-  console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
+ // console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
+ // console.log(LOCAL_FORECAST.tomorrow.max) //same result 84.6
+
+
+  const [a,b,,,...array]=[1,2,3,4,5,6,7,8,9]  // , take one per value & ... should be use for take a array
+  //console.log(array)
+
+  function getName(){
+      class Veletables{
+          constructor (value){
+              this.name=value;
+          }
+      }
+      return Veletables;
+  }
+
+  const one=getName();
+  const vegetables= new one("banana");
+  console.log(vegetables.name)
