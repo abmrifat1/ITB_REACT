f1d21080e569e3844ec37e0980d3509972ef5bf8
abmrifat1 <rifatabm1@gmail.com>
2019-09-25 20:09:21 +0600

    Class

--- day9.js
+++ day9.js
@@ -9,10 +9,54 @@ class Friuts{
     }
 }
 
+Friuts.prototype.size=15;
+
 let friuts= new Friuts("mango",120,"summer");
 
-//friuts.hello("ki");
+//friuts.hello("banana"); //show name is banana
 
-console.log(friuts.name)
+//console.log(friuts.name)
+//console.log(friuts.size)
 
-console.log(friuts instanceof Friuts)
+//console.log(friuts instanceof Friuts)
+
+
+
+const getObjectPorperties=(object)=>{
+    let value="";
+    for(let eachKey in object){
+       
+      value=(value+eachKey+":"+ object[eachKey]+",");
+      
+        
+    }
+    return remove(value);
+  //  console.log(value)
+
+}
+function remove(value){
+    value=value.split("");
+    value.pop();
+    value.push(".")
+    value=value.join("")
+return value
+}
+
+//console.log(getObjectPorperties(friuts))
+
+
+const flowers={
+    info:{
+        name:{
+            scienctificName:"banana_ico"
+        }
+    },
+    color:{
+        rgb:{
+            red:5,
+            green:9,
+            blue:4
+        }
+    }
+}
+
